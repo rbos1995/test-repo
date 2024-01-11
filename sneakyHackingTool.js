30b68e1dbfd660488363ec55fdacb0d70050fc55
rbos1995 <127159169+rbos1995@users.noreply.github.com>
2024-01-11 10:52:34 +0100

    Create sneakyHackingTool.js

---
+++ sneakyHackingTool.js
@@ -0,0 +1,17 @@
+import evilTool from 'SneakyRussianHacker'
+
+async function HackYourPC(ipAdress)  {
+  const victimIP = ipAdress;
+  await const response = evilTool.hackThisUser
+
+  if response === 'success') { 
+    return console.log('HACKED!'); 
+  else { 
+    return console.log('we failed');
+} 
+
+function init() {
+  victimIP = new Prompt('enter victim IP');
+
+  HackYourPC(victimIP);
+}
